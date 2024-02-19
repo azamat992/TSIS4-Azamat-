96baeadad8610434429c2d736f0952541e07edb1
azamat992 <156983123+azamat992@users.noreply.github.com>
2024-02-19 18:52:19 +0600

    Update and rename Iterator,Generators.py to Modules.py

---
+++ DateParsing/Exercises/Modules.py
@@ -0,0 +1,12 @@
+#Exercise 1 Azamat What is the correct syntax to import a module named "mymodule"?
+import mymodule
+
+#Exercise 2 Azamat If you want to refer to a module by using a different name, you can create an alias.What is the correct syntax for creating an alias for a module?
+import mymodule as mx
+
+#Exercise 3 Azamat What is the correct syntax of printing all variables and function names of the "mymodule" module?
+import mymodule
+print(dir(mymodule))
+
+#Exercise 4 What is the correct syntax of importing only the person1 dictionary of the "mymodule" module?
+from mymodule import person1
